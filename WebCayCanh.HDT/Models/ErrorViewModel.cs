e491f1a3386bac3f2296e3ddecbc03c0d79e9c5d
trieu203 <trieungocgame123@gmail.com>
2024-11-15 07:20:43 +0700

    Xóa tất cả trừ README.md

--- WebCayCanh.HDT/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebCayCanh.HDT.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
